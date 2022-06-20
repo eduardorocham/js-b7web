47445cb3431bcc7fe3be94259e15351125b37987
Eduardo Rocha <97197375+eduardorocham@users.noreply.github.com>
2022-06-20 19:19:39 -0300

    New project

---
+++ arrasta-e-solta/script.js
@@ -0,0 +1,88 @@
+let areas = {
+    a: null,
+    b: null,
+    c: null
+};
+
+//Arrastar:
+document.querySelectorAll('.item').forEach(item =>{
+    item.addEventListener('dragstart', dragStart);
+    item.addEventListener('dragend', dragEnd);
+});
+
+//Soltar:
+document.querySelectorAll('.area').forEach(area =>{
+    area.addEventListener('dragover', dragOver); //Entra
+    area.addEventListener('dragleave', dragLeave); //Sai
+    area.addEventListener('drop', drop); //Solta
+});
+
+document.querySelector('.neutralArea').addEventListener('dragover', dragOverNeutral);
+document.querySelector('.neutralArea').addEventListener('dragleave', dragLeaveNeutral);
+document.querySelector('.neutralArea').addEventListener('drop', dropNeutral);
+
+//Functions item
+function dragStart(e) {
+    e.currentTarget.classList.add('dragging');
+};
+
+function dragEnd(e) {
+    e.currentTarget.classList.remove('dragging');
+};
+
+//Functions area
+function dragOver(e) {
+    if(e.currentTarget.querySelector('.item') === null) {
+        e.preventDefault(); //Permite o drop
+        e.currentTarget.classList.add('hover');
+    };
+};
+
+function dragLeave(e) {
+    e.currentTarget.classList.remove('hover');
+};
+
+function drop(e) {
+    e.currentTarget.classList.remove('hover');
+    
+    if(e.currentTarget.querySelector('.item') === null) {
+        let dragItem = document.querySelector('.dragging');
+        e.currentTarget.appendChild(dragItem);
+        updateAreas();
+    };
+};
+
+function dragOverNeutral(e) {
+    e.preventDefault();
+    e.currentTarget.classList.add('hover');
+};
+
+function dragLeaveNeutral(e) {
+    e.currentTarget.classList.remove('hover');
+};
+
+function dropNeutral(e) {
+    e.currentTarget.classList.remove('hover');
+    let dragItem = document.querySelector('.dragging');
+    e.currentTarget.appendChild(dragItem);
+    updateAreas();
+};
+
+//Logic Functions 
+function updateAreas() {
+    document.querySelectorAll('.area').forEach(area => {
+        let name = area.getAttribute('data-name');
+
+        if(area.querySelector('.item') !== null) {
+            areas[name] = area.querySelector('.item').innerHTML;
+        } else {
+            areas[name] = null;
+        }
+    });
+
+    if(areas.a == 1 && areas.b == 2 && areas.c == 3) {
+        document.querySelector('.areas').classList.add('correct');
+    } else {
+        document.querySelector('.areas').classList.remove('correct');
+    };
+};
